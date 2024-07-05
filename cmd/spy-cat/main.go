c80b6902085f19d58e42f5000005b1e11ebaec9c
Illia Kornyk <krekerbrow@gmail.com>
2024-07-05 12:33:59 +0300

    feat: logger

--- cmd/spy-cat/main.go
+++ cmd/spy-cat/main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
-	"log"
+	"log/slog"
+	"os"
 
 	"github.com/illiakornyk/spy-cat/internal/config"
 )
 
+const (
+    envLocal = "local"
+    envDev   = "dev"
+    envProd  = "prod"
+)
+
+
 func main() {
-	config := config.MustLoad()
-	log.Printf("Loaded config: %+v\n", config)
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+    log = log.With(slog.String("env", cfg.Env))
+
+    log.Info("initializing server", slog.String("address", cfg.Address))
+    log.Debug("logger debug mode enabled")
+}
+
+
+
+func setupLogger(env string) *slog.Logger {
+    var log *slog.Logger
+
+    switch env {
+    case envLocal:
+        log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+    case envDev:
+        log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+    case envProd:
+        log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+    }
+
+    return log
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/illiakornyk/spy-cat
 
 go 1.22.2
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8
+	gopkg.in/yaml.v2 v2.4.0
+)
