97a5430f4bacad878abef8f8e75d713383896408
Illia Kornyk <krekerbrow@gmail.com>
2024-07-07 22:49:29 +0300

    fix: delete mission

--- internal/http-server/handlers/missions/delete.go
+++ internal/http-server/handlers/missions/delete.go
@@ -1,6 +1,7 @@
 package missions
 
 import (
+	"errors"
 	"log/slog"
 	"net/http"
 	"strconv"
@@ -23,30 +24,30 @@ func DeleteHandler(logger *slog.Logger, missionDeleter MissionDeleter) http.Hand
 		id, err := strconv.ParseInt(idStr, 10, 64)
 		if err != nil {
 			logger.Error("invalid mission id", slog.Any("error", err))
-			utils.WriteError(w, http.StatusBadRequest, err)
+			utils.WriteError(w, http.StatusBadRequest, errors.New("invalid mission id"))
 			return
 		}
 
 		exists, err := missionDeleter.MissionExists(id)
 		if err != nil {
 			logger.Error("failed to check if mission exists", slog.Any("error", err))
-			utils.WriteError(w, http.StatusInternalServerError, err)
+			utils.WriteError(w, http.StatusInternalServerError, errors.New("failed to check if mission exists"))
 			return
 		}
 		if !exists {
 			logger.Error("mission does not exist", slog.Int64("missionID", id))
-			utils.WriteError(w, http.StatusNotFound, err)
+			utils.WriteError(w, http.StatusNotFound, errors.New("mission does not exist"))
 			return
 		}
 
 		err = missionDeleter.DeleteMission(id)
 		if err != nil {
 			logger.Error("failed to delete mission", slog.Any("error", err))
-			utils.WriteError(w, http.StatusInternalServerError, err)
+			utils.WriteError(w, http.StatusInternalServerError, errors.New("failed to delete mission"))
 			return
 		}
 
 		logger.Info("mission deleted successfully", slog.Int64("id", id))
-		utils.WriteJSON(w, http.StatusNoContent, nil)
+		w.WriteHeader(http.StatusNoContent)
 	}
 }
